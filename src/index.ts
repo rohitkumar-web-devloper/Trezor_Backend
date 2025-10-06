33041725f28f378258e957ac58dadf2e80c2257f
rohitkumar-web-devloper <113985385+rohitkumar-web-devloper@users.noreply.github.com>
2025-10-06 15:43:47 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors({
-    origin: ["https://s3demo.vercel.app" ,"https://ledger-six-sigma.vercel.app/"],
+    origin: ["https://s3demo.vercel.app" ,"https://ledger-six-sigma.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     credentials: true,
 }));
